5936a52ecf4cf5aef270e3b270d719a717ac59f8
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-18 12:15:49 +0530

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Rishav Gupta on 18/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
