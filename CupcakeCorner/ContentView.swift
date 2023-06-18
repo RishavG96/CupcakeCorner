b38d1dfdd6e1b45b656e1343ad8776e98e621c42
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-18 13:28:25 +0530

    Taylor Swift songs
    Taylor Swift songs added to List

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,17 +7,85 @@
 
 import SwiftUI
 
+class User: ObservableObject, Codable {
+    var name = "Paul Hudson" // string conforms to codable out of the box
+    @Published var name1 = "Paul Hudson" // will not compile - because of Published named property wrapper. Name1 property is automatically wrapped inside another type. In this case it is going to announce any any swift ui views which is watching this
+    // In the case of @Published property wrapper, this this is its own struct called Published, and I can store any kind of value. Published struct uses generics. You can make an instance of Published with a type attached to it like Published string or Published Int. You cannot make a Published Object by itself without a type.
+    // Swift already have rules in place that if an array contains codable types then the array itself is also codable. then you can read and write arrays. Same as Dict & Sets.But not for Published.
+    // So we have to make Published conform to Codable explictely. Tell swift which properties should be loaded and saved and how to do both those actions.
+    // Enum that conforms to a special protocol called CodingKey, this means every case of our enum is a value that should be coded (aerchived and unarchived)
+    
+    enum CodingKeys: CodingKey {
+        case name1
+    }
+    
+    // Next, Make some kind of custom initializer given some kind of container and it would use that to read values of all out properties in this case
+    
+    required init(from decoder: Decoder) throws { // gives Decoder which has all kinds of data for us to read it.
+        // required init makes use of the case when if anyone who subclasses our User class with a custom impl they must override this init to add their own values
+        let container = try decoder.container(keyedBy: CodingKeys.self) // Ask the decoder for the container which will have the keys of our CodingKeys enum
+        name = try container.decode(String.self, forKey: .name1) // Find a string and decode it  for key .name1
+    }
+    
+    // We made the above the above init to decode but we need to tell Swift to encode the type as well
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name1, forKey: .name1)
+    }
+    
+    // now our code compiles.
+}
+
+struct Response: Codable {
+    var results: [Result]
+    
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+                    .font(.headline)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+            // ack with await that this func might go to sleep
+        }
+    }
+    
+    func loadData() async { // writing async tell it that this loadData method here might want to go to sleep in order for it to complete it work. This function cannot be be called on onAppear method as that requires a sync function. SwiftUI provides a task modifier for this. This can call functions that can go to sleep
+        
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid Data")
+        }
     }
 }
+
+// Downloads data, caches it locally, and updates the UI view  to display the image when its ready
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
