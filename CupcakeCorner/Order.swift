d95e2c38861de7132aa089bf09bf808af4a33c85
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-18 18:52:42 +0530

    Adding the cupcake and Address view

---
+++ CupcakeCorner/Order.swift
@@ -0,0 +1,38 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Rishav Gupta on 18/06/23.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+    
+    @Published var name = ""
+    @Published var streetAddress = ""
+    @Published var city = ""
+    @Published var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
+}
